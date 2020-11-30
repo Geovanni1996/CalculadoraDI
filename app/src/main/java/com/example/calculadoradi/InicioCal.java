2a3557b4a82539da2cf977feab21ba73b913c0e2
Geovanni Angulo <74083684+Geovanni1996@users.noreply.github.com>
2020-11-30 14:53:40 -0600

    Initial commit

---
+++ app/src/main/java/com/example/calculadoradi/InicioCal.java
@@ -0,0 +1,14 @@
+package com.example.calculadoradi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InicioCal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inicio_cal);
+    }
+}
---
+++ app/src/main/java/com/example/calculadoradi/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.calculadoradi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
